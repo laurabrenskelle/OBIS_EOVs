f52a425376c724ff561d57f7b40ec2f6e1735e5b
Laura Brenskelle <160157051+laurabrenskelle@users.noreply.github.com>
2024-09-18 15:44:35 -0400

    Update EOV_OBIS.R

--- EOV_OBIS.R
+++ EOV_OBIS.R
@@ -4,26 +4,17 @@ library(robis)
 library(dplyr)
 library(htmlwidgets)
 
-# first we will pull the files where the EOV taxonomy are stored from GitHub
-repo_files <- gh("GET /repos/:owner/:repo/contents/:path",
-                 owner = "ioos",
-                 repo = "marine_life_data_network",
-                 path = "eov_taxonomy")
-
-for (file_info in repo_files) {
-  if (file_info$type == "file") {
-    download.file(file_info$download_url, destfile = file_info$name)
-  }
-}
-
 # let's try mangroves as our first example
-mangroves <- read.csv("mangroves.csv")
+# first we will pull the files where the EOV taxonomy are stored in the Marine Life Data Network GitHub
+mangroves <- read.csv("https://raw.githubusercontent.com/ioos/marine_life_data_network/main/eov_taxonomy/mangroves.csv")
+
+# doing some cleanup to get a list of aphiaIDs for our robis query
 mangroves$ID <- gsub("urn:lsid:marinespecies.org:taxname.", "", mangroves$acceptedTaxonId)
 mangroves$ID <- as.numeric(mangroves$ID)
 mangroveIdentifiers <- paste(mangroves$ID, collapse = ", ")
-print(mangroveIdentifiers)
 
-# using the taxonIDs, let's search for occurrence data for this EOV
+# using the taxonIDs from the last step, let's search for occurrence data for mangroves
+# this step may take a while
 mangrove_occ <- robis::occurrence(taxonid = mangroveIdentifiers)
 # let's check how many occurrences we got from OBIS
 nrow(mangrove_occ)
